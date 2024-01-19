8c3387cb880005f6444ca4e2a3f269f9f6ab6a96
dulmul <87592826+dulmul@users.noreply.github.com>
2024-01-19 10:20:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['94786161109', 'HASHEN', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
